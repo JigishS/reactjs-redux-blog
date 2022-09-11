5ad2c283bb0a80ba107474a37b350d802bc77e1e
Jigish Solanki <jigishsolanki111@gmail.com>
2022-09-11 09:36:16 +0530

    Fetch post user - Optimize API

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,15 +1,9 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchUser } from "../actions";
 
 class UserHeader extends React.Component {
-  componentDidMount() {
-    this.props.fetchUser(this.props.userId);
-  }
-
   render() {
     const { user } = this.props;
-    console.log(user);
     if (!user) {
       return null;
     }
@@ -22,4 +16,4 @@ const mapStateToProps = (state, ownProps) => {
   return { user: state.users.find((user) => user.id === ownProps.userId) };
 };
 
-export default connect(mapStateToProps, { fetchUser })(UserHeader);
+export default connect(mapStateToProps)(UserHeader);
