2a243db92347a3dd4b402034cd4c5c422a2444c7
Jigish Solanki <jigishsolanki111@gmail.com>
2022-09-11 09:56:11 +0530

    Get user - Optimized method

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,8 +4,17 @@ import jsonPlaceholder from "../apis/jsonPlaceholder";
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
 
+  /*
   const userIds = _.uniq(_.map(getState().posts, "userId"));
   userIds.forEach((id) => dispatch(fetchUser(id)));
+  */
+
+  // Optimized method
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach((id) => dispatch(fetchUser(id)))
+    .value();
 };
 
 export const fetchPosts = () => async (dispatch) => {
